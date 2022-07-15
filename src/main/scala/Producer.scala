11000cc2842d0b3e9d62cd2c33f4e91742059078
Deepak <j.thepac@gmail.com>
2022-07-15 10:06:29 +0530

    Update Producer.scala

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -5,17 +5,14 @@ import org.apache.kafka.clients.producer._
 
 object Producer {
 
-  def main(args: Array[String]): Unit =
-    writeToKafka("quickstart")
-  def writeToKafka(topic: String): Unit = {
-
+  def main(args: Array[String]): Unit ={
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:9092") //copy  from kafka  ie., server port = 9092
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
     val producer = new KafkaProducer[String, String](props)
-    val record = new ProducerRecord[String, String](topic, "Deepak key", " Deepak value")
+    val record = new ProducerRecord[String, String]("quickstart", "Deepak key", " Deepak value")
     producer.send(record)
     producer.close()
     println("Sent !!")
